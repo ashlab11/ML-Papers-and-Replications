4f5c93161146abc96ede088194c18d622321a3b1
ashlab11 <asherlabovich@gmail.com>
2025-01-20 01:40:17 +1100

    Added paper on FFNN

--- ML_From_Scratch/Neural Network/Testing_Dense.py
+++ ML_From_Scratch/Neural Network/Testing_Dense.py
@@ -4,6 +4,8 @@ from Layer_Dense import *
 from Activations import *
 from ErrorFunctions import *
 from Network import Network
+from sklearn.model_selection import RandomizedSearchCV
+import xgboost as xgb
 
 np.random.seed(0)
 
@@ -15,13 +17,35 @@ def create_regression_data(n_points, n_features):
 
 #We're using row terminology -- each layer is a row
 X, y = create_regression_data(1000, 3)
+X_test, y_test = create_regression_data(100, 3)
+
+#Run xgboost on this as well
+param_dist = {
+    'n_estimators': [100, 500, 1000],
+    'learning_rate': [0.01, 0.05, 0.1, 0.3],
+    'max_depth': [3, 5, 7, 9],
+    'min_child_weight': [1, 3, 5],
+    'subsample': [0.6, 0.8, 1.0],
+    'colsample_bytree': [0.6, 0.8, 1.0],
+    'gamma': [0, 0.1, 0.2, 0.3],
+    'reg_alpha': [0, 0.1, 0.5, 1.0],
+    'reg_lambda': [1.0, 1.5, 2.0]
+}
+
+search = RandomizedSearchCV(xgb.XGBRegressor(), param_dist, n_iter = 50, n_jobs = -1, cv = 3, verbose = 1, random_state = 0)
+search.fit(X, y)
+predictions = search.predict(X_test)
+
+print(np.mean((predictions - y_test) ** 2))
 
 #What we want the network to look like
-network = Network(layers = [
+"""network = Network(layers = [
     Layer_Input(3), #Input layer
-    Layer_Dense(3, 5, Activation_ReLU()),
-    Layer_Dense(5, 1, Activation_None()), #Output layer
+    Layer_Dense(3, 10, Activation_ReLU()),
+    Layer_Dense(10, 10, Activation_ReLU()),
+    Layer_Dense(10, 1, Activation_None()), #Output layer
 ], error_func = MSE())
 
-network.train(X, y, epochs=100)
-network.predict(X)
+network.train(X, y, epochs=1000)
+predictions = network.predict(X_test)
+print(np.mean((predictions - y_test) ** 2))"""
